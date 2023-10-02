764799d6498b77af06e69c2ee4164d152fece3ed
julidavi777 <93684468+julidavi777@users.noreply.github.com>
2023-10-02 06:44:35 -0500

    Update index.blade.php

--- resources/views/empleados/index.blade.php
+++ resources/views/empleados/index.blade.php
@@ -1,24 +1,30 @@
-<x-layouts.main-layout>
-    <div class="container">
-        <h1>Listado de Empleados</h1>
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Apellidos</th>
-                    <th>Cédula</th>
-                    <th>Dirección</th>
-                    <th>Departamento</th>
-                    <th>Municipio</th>
-                    <th>Barrio</th>
-                    <th>Tipo de Empleado</th>
-                    <th>Salario</th>
-                </tr>
-            </thead>
-            
-        </table>
-    </div>
+<x-layouts.main-layout> 
+    <div class="container"> 
+        <h1>Listado de Empleados</h1> 
+ 
+        <table class="table"> 
+            <thead> 
+                <tr> 
+                    <th>ID</th> 
+                    <th>Nombre</th> 
+                    <th>Apellidos</th> 
+                    <th>Cédula</th> 
+                    <th>Dirección</th> 
+                    <th>Departamento</th> 
+                    <th>Municipio</th> 
+                    <th>Barrio</th> 
+                    <th>Tipo de Empleado</th> 
+                    <th>Salario</th> 
+                    <th><a href="{{url('empleados/edit')}}" class="btn btn-primary mt-3">Editar</a></th> 
+                </tr> 
+            </thead> 
+             
+        </table> 
+ 
+     
+    <a href="{{url('empleados/create')}}" class="btn btn-primary mt-3">Crear Empleado</a> 
+     
+ 
+ 
+    </div> 
 </x-layouts.main-layout>
-
